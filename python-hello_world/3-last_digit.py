7254a1663b687d16a6fb0e1425d51c1564d44794
Paul <oyediranp@gmail.com>
2023-09-16 09:47:22 +0100

    my first commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,12 +7,12 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
     
-if last_digit > 5:
-     s = "and is greater than 5"
-elif last_digit == 0:
-     s = "and is 0"
-elif last_digit < 6 and not 0:
-     s = "and is less than 6 and not 0"
-     
+print('Last digit of {} is {}'.format(number, last_digit), end=" ")
 
-print('Last digit of {} is {} {}'.format(number, last_digit, s))
+if last_digit > 5:
+     print("and is greater than 5")
+elif last_digit == 0:
+     print("and is 0")
+else:
+     print("and is less than 6 and not 0")
+     
---
+++ python-hello_world/README.md
@@ -0,0 +1,2 @@
+# Developing a skill in python
+I am learn the python skill through alx classes
