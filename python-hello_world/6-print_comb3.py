071e2d1a9278b71aca223251a97e6a984c0dae26
Paul <oyediranp@gmail.com>
2023-09-16 16:39:41 +0100

    my first commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,10 @@
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        print("{}{}".format(first_digit, second_digit), end=", ")
+        if first_digit == 8 and second_digit == 9:
+            print("{}{}".format(first_digit, second_digit), end="")
+        else:
+            print("{}{}".format(first_digit, second_digit), end=", ")
+          
 
 
 print()
